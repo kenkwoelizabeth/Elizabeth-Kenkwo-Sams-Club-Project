748651111cfcf00dd46b0025c21aed7fb77132bd
Elizabeth <kenkwoelizabeth78@gmail.com>
2023-10-19 19:04:29 -0400

    first commit

---
+++ src/main/java/com/samsclub/SamsClubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.samsclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsClubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsClubProjectApplication.class, args);
+	}
+
+}
